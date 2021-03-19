b741ea02d0236974fe5f63f717a895021370cb66
A01753328 <A01753328@itesm.mx>
2021-03-19 08:31:28 -0600

    Actualizacion de la convolucion

--- convolucion.py
+++ convolucion.py
@@ -1,9 +1,10 @@
 import numpy as np
+import cv2
 
 def convolucion(imagenO, kernel):
     fr = len(imagenO) - (len(kernel)-1)
     cr = len(imagenO[0]) - (len(kernel[0])-1)
-    matrizR = np.zeros((fr,cr))
+    matrizR = np.zeros((fr,cr),np.uint8)
 
     #For para recorrer filas
     for i in range(len(matrizR)):
@@ -14,9 +15,13 @@ def convolucion(imagenO, kernel):
             for m in range(len(kernel)):
                 for n in range(len(kernel[0])):
                     suma += kernel[m][n] * imagenO[m+i][n+j]
-            matrizR[i][j] = suma
+            if suma<=255:
+                matrizR[i][j] = round(suma)
+            else:  
+                matrizR[i][j] = 255
     return matrizR
 
+#imagenes
 K = [[-1,0,1],[-1,0,1],[-1,0,1]]
 I = [[2,0,1,1,1],[3,0,0,0,2],[1,1,1,1,1],[3,1,1,1,2],[1,1,1,1,1]]
 
@@ -25,8 +30,12 @@ I = [[2,0,1,1,1],[3,0,0,0,2],[1,1,1,1,1],[3,1,1,1,2],[1,1,1,1,1]]
 In = np.array(I)
 Kn = np.array(K)
 
-#funcion de convolucion
-R = convolucion(In,Kn)
-print(R)
+IRGB = cv2.imread("R2D2.jpg")
+IGS = cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
+print(IGS.shape)
 
+#funcion de convolucion
+R = convolucion(IGS,Kn)
+print(R)
+cv2.imwrite("R2D2C.jpg",R)
             
